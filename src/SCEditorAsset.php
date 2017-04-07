006d0bbe56c0a0135d040ecb68d36faeb8704a6b
Shawn Wilson <swilson1337@gmail.com>
2017-04-07 12:07:57 -0400

    Update SCEditorAsset.php

--- src/SCEditorAsset.php
+++ src/SCEditorAsset.php
@@ -4,5 +4,13 @@ namespace swilson1337\sceditor;
 
 class SCEditorAsset extends \yii\web\AssetBundle
 {
-	public $sourcePath = '@vendor/swilson1337/yii2-widget-sceditor/assets';
+	public $sourcePath = '@vendor/bower/sceditor';
+	
+	public $js = [
+		'minified/jquery.sceditor.bbcode.min.js',
+	];
+	
+	public $css = [
+		'minified/themes/default.min.css',
+	];
 }
